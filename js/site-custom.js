a9c6f3425aeb3d5aa1454b480196d479e5d25dc5
Jean-Paul van Houten <jeanpaulvanhouten@gmail.com>
2019-02-10 13:38:02 +0100

    added coffee calculator

--- js/site-custom.js
+++ js/site-custom.js
@@ -329,6 +329,17 @@
 	        },
 		}); /*========================================*/
 
-		$("#coffeecounter").html(500);
+		function workday_count(start, end) {
+			var first = start.clone().endOf('week'); // end of first week
+			var last = end.clone().startOf('week'); // start of last week
+			var days = last.diff(first, 'days') * 5 / 7; // this will always multiply of 7
+			var wfirst = first.day() - start.day(); // check first week
+			if (start.day() == 0)--wfirst; // -1 if start with sunday 
+			var wlast = end.day() - last.day(); // check last week
+			if (end.day() == 6)--wlast; // -1 if end with saturday
+			return wfirst + days + wlast; // get the total
+		}
+
+		$("#coffeecounter").html(workday_count('2014-02-01', new Date().toISOString().slice(0, 10)) * 0.80);
 		
 	});
